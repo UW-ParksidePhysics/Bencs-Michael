e3b4e858d0d0aad1bfaadc3ceac5f08ae9e6f9b8
bencs004 <bencs004@rangers.uwp.edu>
2020-04-05 15:40:17 -0500

    add 3.3

--- 3.6_trapezint_01.py
+++ 3.6_trapezint_01.py
@@ -1,9 +1,9 @@
 from math import *
 def trapezint1(f, a, b, actual_area):
     A = ((b - a) / 2) * (f(a) + f(b))
-    print("Area under the curve =", A)
+    print("Area beneath the curve =", A)
     error = abs(A) - abs(actual_area)
-    print("Error =", error)
+    print("Your Error =", error)
     return trapezint1
 
 def f(x):
@@ -34,7 +34,7 @@ print()
 def trapezint2(f, a, b, actual_area):
     c = b / 2
     A = ((c - a) / 2) * (f(a) + f(c)) + ((b - c) / 2) * (f(c) + f(b))
-    print("Area under the curve =", A)
+    print("Area Beneath the curve =", A)
     error = abs(A) - abs(actual_area)
     print("Error =", error)
     return trapezint2
@@ -64,8 +64,7 @@ actual_area = 1
 trapezint2(h, a, b, actual_area)
 print()
 
-
-def trapezint(f, a, b, n, actual_area): #
+def trapezint(f, a, b, n, actual_area):
     x = a; A = 0; i = 0; h = (b - a) / n
 
     while i <= n:
@@ -73,7 +72,7 @@ def trapezint(f, a, b, n, actual_area): #
         A += del_a
         x += h
         i += 1
-    print("Area under the curve =", A)
+    print("Area Beneath the curve =", A)
     error = abs(A) - actual_area
     print("Error =", error)
     return trapezint
