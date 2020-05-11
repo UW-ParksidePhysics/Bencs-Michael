5f0695ea2c8d578d5ff50189144d112ec71787fc
bencs004 <bencs004@rangers.uwp.edu>
2020-05-11 00:24:35 -0500

    Python final

--- Final_module.py
+++ Final_module.py
@@ -1,3 +1,6 @@
-from two_column_text_read import two_column_text_read
+from two_column_text_read import two_column_text_read, x_column, y_column
 
-str = two_column_text_read(volumes_energies.txt, 2)
+str = two_column_text_read('volumes_energies.txt')
+
+x= x_column(str)
+print(x)
--- two_column_text_read.py
+++ two_column_text_read.py
@@ -1,10 +1,27 @@
 # reads data file
-def two_column_text_read(file, column):
-    infile= open('file', 'r')
-    lines= infile.readlines()
-    result= []
-    for number in lines:
-        result.append(number.split(' ')[1])
-    infile.close()
+def two_column_text_read(str):
+    infile = open(str, 'r').readlines()
 
 
+def x_column(infile):
+    two_column_text_read(str)
+    xlines = infile.readlines()
+    result = []
+    for x in xlines:
+        result.append(x.split('  ')[1])
+        print(x)
+
+
+def y_column(y):
+    lines = infile.readlines()
+    result = []
+    for y in lines:
+        result.append(y.split('  ')[2])
+        print(y)
+
+#    print(infile)
+
+
+#       column1 = float(lines.split()[2]).append(column1)
+#       column2 = float(lines.split()[2]).append(column2)
+#       print( '% % ' %(column1, column2))
