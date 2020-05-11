99c4827369689a40d612d6143cde7c5d63a7a046
bencs004 <bencs004@rangers.uwp.edu>
2020-05-10 20:38:00 -0500

    Python final

--- two_column_text_read.py
+++ two_column_text_read.py
@@ -1,6 +1,6 @@
 # reads data file
 def two_column_text_read(file, column):
-    infile= open(file, 'r')
+    infile= open('file', 'r')
     lines= infile.readlines()
     result= []
     for number in lines:
